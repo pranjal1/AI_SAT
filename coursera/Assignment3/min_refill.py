d19ab4e77504470fcc5a72d44e66a3883806969c
pranjal1 <pranjal750@gmail.com>
2018-02-14 06:37:28 +0545

    min_ref

--- coursera/Assignment3/min_refill.py
+++ coursera/Assignment3/min_refill.py
@@ -7,18 +7,22 @@ L = 400
 
 
 num_refills = 0
-cur_refill = 0
-last_refill = 0
+cur_pos = 0
+last_pos = 0
+range_left = 400
 
-while (cur_refill <= n):
-	last_refill = cur_refill
-	while(cur_refill <=n and (x[cur_refill + 1] - x[last_refill] <= L)):
-		cur_refill += 1
-	if(cur_refill == last_refill):
-		print "Impossible"
-		sys.exit(0)
-	if (cur_refill <= n):
-		num_refills + 1
+while (cur_pos < n):
+	last_pos = cur_pos
+	while(cur_pos <n and (x[cur_pos + 1] - x[last_pos] <= range_left)):
+		range_left -= (x[cur_pos + 1] - x[last_pos])
+		print range_left
+		cur_pos += 1
+		if(cur_pos == last_pos):
+			print "Impossible"
+			sys.exit(0)
+	
+	num_refills + 1
+	range_left = 400
 
 print num_refills
 
