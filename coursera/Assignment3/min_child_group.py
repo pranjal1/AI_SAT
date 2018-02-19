24a5b2dc0c16aca5dd0a6c7313c72f6ec9748b14
pranjal1 <pranjal750@gmail.com>
2018-02-19 16:41:55 +0545

    added hashing and completed min child

--- coursera/Assignment3/min_child_group.py
+++ coursera/Assignment3/min_child_group.py
@@ -1,6 +1,9 @@
 #using python3
-import sys
+#finding the product of the largest two numbers using the quick sort technique
+#firstly, trying to successfully implement quick sort
 
+
+#this function sorts the array in the given low to high limit
 def partition_sorter(arr,low,high):
     pivot = arr[high]
     index = low-1    
@@ -11,23 +14,30 @@ def partition_sorter(arr,low,high):
             
     return index
 
-	
 
 def quick_sort(arr,low,high):
     if low < high:
         pi = partition_sorter(arr,low,high)
-        quick_sort(arr,low,pi-1)
-        quick_sort(arr,pi+1,high)
-	return arr
-        
-array = [4,4,1,2,2,5,9,4]      
-res = quick_sort(array,0,len(array)-1)
-print res
+        #in every recursion, both of the below 
+        #functions are called by the above
+        #if condition restricts the unnecessary one
+        arr=quick_sort(arr,low,pi-1)
+        arr=quick_sort(arr,pi+1,high)
+    return (arr)
 
-x = [3.2,3.8,4.9,5.8,9.7,17.8,17.9,18.0,22,22.2,24]
+array = [24,3.8,4.9,5.8,9.7,17.8,17.9,18.0,22,22.2,3.2]      
+x = quick_sort(array,0,len(array)-1)
+print (x)  
+    
 
-n = len(x) - 1 
+
+#x = [3.2,3.8,4.9,5.8,9.7,17.8,17.9,18.0,22,22.2,24]
+
+
+n = len(x) - 1
+
 L = 2 # max age gap of clildrens
+
 
 
 
@@ -35,13 +45,18 @@ num_groups = 1
 cur_pos = 0
 last_pos = 0
 
+
+
 while (cur_pos <= n):
-	while(cur_pos < n   and (x[cur_pos] - x[last_pos]) <= L):				
+	while(cur_pos < n   and (x[cur_pos] - x[last_pos]) <= L):
 		cur_pos += 1
-	if((x[cur_pos] - x[last_pos]) > L):		
-		num_groups += 1
+		
+		if((x[cur_pos] - x[last_pos]) > L):
+			num_groups += 1
+			
 	last_pos = cur_pos
 	cur_pos += 1
+
 
 print ('the minimum number of groups are',num_groups)
 
