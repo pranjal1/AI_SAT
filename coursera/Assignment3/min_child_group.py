21bb0e701b66265fc581813db6777140da820162
pranjal1 <pranjal750@gmail.com>
2018-02-19 07:26:35 +0545

    min child group

--- coursera/Assignment3/min_child_group.py
+++ coursera/Assignment3/min_child_group.py
@@ -1,10 +1,10 @@
 #using python3
 import sys
 
-x = [0,200,375,550,750,950]
+
+x = [3.2,3.8,4.9,5.8]
 n = len(x) -1 
-print n
-L = 400
+L = 1
 
 
 num_refills = 0
@@ -14,18 +14,14 @@ range_left = L
 
 while (cur_pos < n-1):
 	while(cur_pos < n and (x[cur_pos+1] - x[last_pos]) <= range_left):		
-		range_left -= (x[cur_pos+1] - x[last_pos])		
+		range_left -= (x[cur_pos+1] - x[last_pos])
+		print x[cur_pos]		
 		cur_pos += 1		
 		last_pos = cur_pos
-			
-	if((x[cur_pos+1] - x[last_pos]) > L):
-		print 'cannot move forward from position ',cur_pos 
-		sys.exit(0)	
-	print 'Stop @ ',cur_pos,x[cur_pos]
+	print '======================'			
 	num_refills += 1
 	range_left = L
 
-print num_refills 
-
+print num_refills + 1 
 
 
