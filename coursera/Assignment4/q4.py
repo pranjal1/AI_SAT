1349cba024a441cae1ca4435c36857b5a3b51f12
pranjal1 <pranjal750@gmail.com>
2018-03-28 14:54:05 +0545

    workable but incorrect q4 assig 4

--- coursera/Assignment4/q4.py
+++ coursera/Assignment4/q4.py
@@ -1,12 +1,12 @@
 # Uses python3
 import sys
-
+num_invs = 0
 # sum of values of variable j is answer, figure out the way
 
-def merge(left, right):
+def compare_and_merge(left, right):
 	if not len(left) or not len(right):
 		return left or right
-
+	print (left,right)
 	result = []
 	i, j = 0, 0
 	while (len(result) < len(left) + len(right)):
@@ -14,33 +14,34 @@ def merge(left, right):
 			result.append(left[i])
 			i+= 1
 		else:
+			print(left[i],right[j])
 			result.append(right[j])
 			j+= 1
+			global num_invs
+			num_invs +=1
 		if i == len(left) or j == len(right):
 			result.extend(left[i:] or right[j:])
 			break 
+		print("================================")	
 	return result
 
-def mergesort(list):
+def bin_divide(list):
 	if len(list) < 2:
 		return list
 
 	middle = len(list)//2
-	#print (list[:middle])
-	left = mergesort(list[:middle])
-	#print (list[middle:])
-	right = mergesort(list[middle:])
-
-	return merge(left, right)
+	left = bin_divide(list[:middle])
+	right = bin_divide(list[middle:])
+	return compare_and_merge(left, right)
 
 if __name__ == '__main__':
 	input = sys.stdin.read()
 	data = list(map(int, input.split()))
 	a = data[1:]
 	n = int(data[0])
-	#print (a)
-	print(mergesort(a))
-	print (num_inversions)
+	sort_list = bin_divide(a)
+	print (num_invs)
+	print(sort_list)
 
 
 
